83f0b250c19c558ad30d0d3fabaec083b2fe500b
gracellandrn <gracellaandreni@gmail.com>
2025-06-02 12:58:19 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import Typed from "typed.js";
+
+const myTyped = new Typed("#greeting", {
+    strings: ["Software Engineer.", "Fullstack Developer."],
+    loop: true,
+    typeSpeed: 25
+})
+
+window.toggleMenu = function () {
+    document.getElementById("mobile-menu").classList.toggle("hidden");
+}
+
+const toggleDark = document.getElementById('toggleDark');
+toggleDark.addEventListener('click', () => {
+    document.documentElement.classList.toggle('dark');
+    localStorage.theme = document.documentElement.classList.contains('dark') ? 'dark' : 'light';
+});
+
+if (localStorage.theme === 'dark') {
+    document.documentElement.classList.add('dark');
+} else {
+    document.documentElement.classList.remove('dark');
+}
