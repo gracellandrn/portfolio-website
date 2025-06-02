74c0106abdbbf9051278c5ed2ff816dea6c00327
gracellandrn <gracellaandreni@gmail.com>
2025-06-02 19:10:02 +0700

    update dark mode

--- src/main.js
+++ src/main.js
@@ -16,7 +16,8 @@ toggleDark.addEventListener('click', () => {
     localStorage.theme = document.documentElement.classList.contains('dark') ? 'dark' : 'light';
 });
 
-if (localStorage.theme === 'dark') {
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches)) {
     document.documentElement.classList.add('dark');
 } else {
     document.documentElement.classList.remove('dark');
